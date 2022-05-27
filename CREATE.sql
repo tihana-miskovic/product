90f00e22d90da898326a4ec3a49b2cf58a3f9337
tihana-miskovic <tihana.miskovic@me.com>
2022-05-27 15:31:03 +0200

    Update CREATE.sql

--- CREATE.sql
+++ CREATE.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.product
 (
-    id integer NOT NULL DEFAULT nextval('product_id_seq'::regclass),
+    id SERIAL NOT NULL,
     code character varying(10) COLLATE pg_catalog."default",
     description character varying(255) COLLATE pg_catalog."default",
     is_available boolean NOT NULL,
